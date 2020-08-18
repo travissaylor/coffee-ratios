a6c198bb94c4bf4436902798c59ea8387950ecb3
Travis Saylor <tsaylor@Traviss-MBP.attlocal.net>
2020-08-17 19:08:57 -0500

    Removing old unused imports

--- components/ui/Card.js
+++ components/ui/Card.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View, TextInput, Button, TouchableOpacity } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import QuantityTitle from './QuantityTitle';
 import IncrementButton from './IncrementButton';
 import DecrementButton from './DecrementButton';
